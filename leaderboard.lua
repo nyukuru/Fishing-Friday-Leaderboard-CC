ea74c6522a47f0410deee817dc92f3261357ad55
nyukuru <97425873+nyukuru@users.noreply.github.com>
2025-10-11 03:06:27 -0400

    Initial commit

---
+++ leaderboard.lua
@@ -0,0 +1,117 @@
+--[[
+MIT License
+
+Copyright (c) 2025 nyukuru
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
+
+BACKGROUND_COLOR = {r=20, g=131, b=156}
+FOREGROUND_COLOR = {r=219, g=215, b=204}
+
+HEADS_Y_LEVEL = 60
+HEADS_PER_PAGE = 3
+
+MINECRAFT_HEADS_WIDTH = 8
+
+FOREGROUND_INDEX = 1
+BACKGROUND_INDEX = 2
+
+local colorutils = require("colorutils.colorutils")
+local assets = require("assets")
+
+local fishers = {}
+local palettes = {}
+local current_page = 1
+
+local function render_page(page)
+    local start_index = (page - 1) * HEADS_PER_PAGE + 1
+
+    colorutils.use_palette(palettes[page])
+
+    local mon_width, _ = term.getSize()
+    local available_space = mon_width - (MINECRAFT_HEADS_WIDTH * HEADS_PER_PAGE)
+    local gap_size = available_space / (HEADS_PER_PAGE + 1)
+    for i = 1, math.min(#fishers - start_index, HEADS_PER_PAGE) do
+        local x = gap_size + (MINECRAFT_HEADS_WIDTH + gap_size) * (i - 1)
+        paintutils.drawImage(fishers[start_index + i - 1].image, math.ceil(x + 0.5), HEADS_Y_LEVEL)
+    end
+end
+
+local function generate_heads_palette(page)
+    local all_pixels = {}
+    local index = 1
+    local start_index = (page - 1) * HEADS_PER_PAGE + 1
+
+    for i = start_index, math.min(#fishers, start_index + HEADS_PER_PAGE - 1) do
+        for _, row in ipairs(fishers[i].pixel_table) do
+            for _, pixel in ipairs(row) do
+                all_pixels[index] = pixel
+                index = index + 1
+            end
+        end
+    end
+
+    palettes[page] = colorutils.generate_palette_with_statics(all_pixels, FOREGROUND_COLOR, BACKGROUND_COLOR)
+
+    for i = start_index, math.min(#fishers, start_index + HEADS_PER_PAGE - 1) do
+        fishers[i].image = colorutils.quantize_image(fishers[i].pixel_table, palettes[page])
+    end
+end
+
+local function main()
+    local monitor = peripheral.find("monitor")
+    local terminal = term.native()
+
+    monitor.setTextScale(0.5)
+
+    term.redirect(monitor)
+    term.setBackgroundColor(BACKGROUND_INDEX)
+    term.setTextColor(FOREGROUND_INDEX)
+    term.setPaletteColor(FOREGROUND_INDEX, colorutils.pack_rgb(FOREGROUND_COLOR))
+    term.setPaletteColor(BACKGROUND_INDEX, colorutils.pack_rgb(BACKGROUND_COLOR))
+    term.clear()
+
+    paintutils.drawImage(paintutils.parseImage(assets.banner), 2, 2)
+
+    term.redirect(terminal)
+
+    while true do
+        term.clear()
+        print("Enter your username to recieve your fishing license for this week: ")
+        local username = string.lower(read())
+
+        local f = io.open(username, "r")
+        if f then
+            io.close(f)
+            table.insert(fishers, {username=username, pixel_table=colorutils.load_image_data(username)})
+        else
+            table.insert(fishers, {username=username, pixel_table=colorutils.generate_head_image_data(username)})
+        end
+
+        current_page = math.floor(#table / HEADS_PER_PAGE) + 1
+        generate_heads_palette(current_page)
+
+        term.redirect(monitor)
+        render_page(current_page)
+        term.redirect(terminal)
+    end
+end
+
+main()
---
+++ pngutils/pngutils.lua
@@ -0,0 +1,221 @@
+local Class = require('pngutils.30log')
+local deflate = require('pngutils.deflate')
+local Stream = require('pngutils.stream')
+
+local Chunk = Class()
+Chunk.__name = "Chunk"
+Chunk.length = 0
+Chunk.name = ""
+Chunk.data = ""
+Chunk.crc = ""
+
+function Chunk:init(stream)
+	if stream.__name == "Chunk" then
+		self.length = stream.length
+		self.name = stream.name
+		self.data = stream.data
+		self.crc = stream.crc
+	else
+		self.length = stream:readInt()
+		self.name = stream:readChars(4)
+		self.data = stream:readChars(self.length)
+		self.crc = stream:readChars(4)
+	end
+end
+
+function Chunk:getDataStream()
+	return Stream({input = self.data})
+end
+
+local IHDR = Chunk:extend()
+IHDR.__name = "IHDR"
+IHDR.width = 0
+IHDR.height = 0
+IHDR.bitDepth = 0
+IHDR.colorType = 0
+IHDR.compression = 0
+IHDR.filter = 0
+IHDR.interlace = 0
+
+function IHDR:init(chunk)
+	self.super.init(self, chunk)
+	local stream = chunk:getDataStream()
+	self.width = stream:readInt()
+	self.height = stream:readInt()
+	self.bitDepth = stream:readByte()
+	self.colorType = stream:readByte()
+	self.compression = stream:readByte()
+	self.filter = stream:readByte()
+	self.interlace = stream:readByte()
+end
+
+local IDAT = Chunk:extend()
+IDAT.__name = "IDAT"
+
+function IDAT:init(chunk)
+	self.super.init(self, chunk)
+end
+
+local PLTE = Chunk:extend()
+PLTE.__name = "PLTE"
+PLTE.numColors = 0
+PLTE.colors = {}
+
+function PLTE:init(chunk)
+	self.super.init(self, chunk)
+	self.numColors = math.floor(chunk.length/3)
+	local stream = chunk:getDataStream()
+	for i = 1, self.numColors do
+		self.colors[i] = {
+			R = stream:readByte(),
+			G = stream:readByte(),
+			B = stream:readByte(),
+		}
+	end
+end
+
+function PLTE:getColor(index)
+	return self.colors[index]
+end
+
+--Stolen right from w3.
+local function paeth_predict(a, b, c)
+	local p = a + b - c
+	local varA = math.abs(p - a)
+	local varB = math.abs(p - b)
+	local varC = math.abs(p - c)
+	if varA <= varB and varA <= varC then return a end
+	if varB <= varC then return b end
+	return c
+end
+
+local function get_pixel(stream, depth)
+	local bps = math.floor(depth/8)
+	local r = stream:readInt(bps)
+	local g = stream:readInt(bps)
+	local b = stream:readInt(bps)
+	local a = stream:readInt(bps)
+	return {r = r, g = g, b = b}
+end
+
+local function scanline(stream, depth, length)
+	local bpp = math.floor(depth/8) * 4
+	local bpl = bpp*length
+	local filterType = stream:readByte()
+	local pixels = {}
+	stream:seek(-1)
+	stream:writeByte(0)
+	local startLoc = stream.position
+	if filterType == 0 then
+		for i = 1, length do
+			pixels[i] = get_pixel(stream, depth)
+		end
+	end
+	if filterType == 1 then
+		for i = 1, length do
+			for j = 1, bpp do
+				local curByte = stream:readByte()
+				stream:seek(-(bpp+1))
+				local lastByte = 0
+				if stream.position >= startLoc then lastByte = stream:readByte() or 0 else stream:readByte() end
+				stream:seek(bpp-1)
+				stream:writeByte((curByte + lastByte) % 256)
+			end
+			stream:seek(-bpp)
+			pixels[i] = get_pixel(stream, depth)
+		end
+	end
+	if filterType == 2 then
+		for i = 1, length do
+			for j = 1, bpp do
+				local curByte = stream:readByte()
+				stream:seek(-(bpl+2))
+				local lastByte = stream:readByte() or 0
+				stream:seek(bpl)
+				stream:writeByte((curByte + lastByte) % 256)
+			end
+			stream:seek(-bpp)
+			pixels[i] = get_pixel(stream, depth)
+		end
+	end
+	if filterType == 3 then
+		for i = 1, length do
+			for j = 1, bpp do
+				local curByte = stream:readByte()
+				stream:seek(-(bpp+1))
+				local lastByte = 0
+				if stream.position >= startLoc then lastByte = stream:readByte() or 0 else stream:readByte() end
+				stream:seek(-(bpl)+bpp-2)
+				local priByte = stream:readByte() or 0
+				stream:seek(bpl)
+				stream:writeByte((curByte + math.floor((lastByte+priByte)/2)) % 256)
+			end
+			stream:seek(-bpp)
+			pixels[i] = get_pixel(stream, depth)
+		end
+	end
+	if filterType == 4 then
+		for i = 1, length do
+			for j = 1, bpp do
+				local curByte = stream:readByte()
+				stream:seek(-(bpp+1))
+				local lastByte = 0
+				if stream.position >= startLoc then lastByte = stream:readByte() or 0 else stream:readByte() end
+				stream:seek(-(bpl + 2 - bpp))
+				local priByte = stream:readByte() or 0
+				stream:seek(-(bpp+1))
+				local lastPriByte = 0
+				if stream.position >= startLoc - (length * bpp + 1) then lastPriByte = stream:readByte() or 0 else stream:readByte() end
+				stream:seek(bpl + bpp)
+				stream:writeByte((curByte + paeth_predict(lastByte, priByte, lastPriByte)) % 256)
+			end
+			stream:seek(-bpp)
+			pixels[i] = get_pixel(stream, depth)
+		end
+	end
+	return pixels
+end
+
+local pngImage = Class()
+pngImage.__name = "PNG"
+pngImage.width = 0
+pngImage.height = 0
+pngImage.depth = 0
+pngImage.colorType = 0
+pngImage.pixels = {}
+
+function pngImage:init(handle)
+	local str = Stream({handle = handle})
+	if str:readChars(8) ~= "\137\080\078\071\013\010\026\010" then error 'Not a PNG' end
+	local ihdr = {}
+	local plte = {}
+	local idat = {}
+	local num = 1
+	while true do
+		ch = Chunk(str)
+		if ch.name == "IHDR" then ihdr = IHDR(ch) end
+		if ch.name == "PLTE" then plte = PLTE(ch) end
+		if ch.name == "IDAT" then idat[num] = IDAT(ch) num = num+1 end
+		if ch.name == "IEND" then break end
+	end
+	self.width = ihdr.width
+	self.height = ihdr.height
+	self.depth = ihdr.bitDepth
+
+	local dataStr = ""
+	for k,v in pairs(idat) do dataStr = dataStr .. v.data end
+	local output = {}
+	deflate.inflate_zlib {input = dataStr, output = function(byte) output[#output+1] = string.char(byte) end, disable_crc = true}
+	local imStr = Stream({input = table.concat(output)})
+
+	for i = 1, self.height do
+		self.pixels[i] = scanline(imStr, self.depth, self.width)
+	end
+end
+
+function pngImage:getPixel(x, y)
+	local pixel = self.pixels[y][x]
+	return pixel
+end
+
+return pngImage
